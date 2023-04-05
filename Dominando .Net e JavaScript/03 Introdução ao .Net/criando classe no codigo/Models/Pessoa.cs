601f0c5342488396564ca6709f5725c35c5b1cac
AlexSan1910 <alex.gsan11@gmail.com>
2023-04-05 14:31:18 -0300

    Utilização de classe no .net

---
+++ Dominando .Net e JavaScript/03 Introdução ao .Net/criando classe no codigo/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace criando_classe_no_codigo.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
