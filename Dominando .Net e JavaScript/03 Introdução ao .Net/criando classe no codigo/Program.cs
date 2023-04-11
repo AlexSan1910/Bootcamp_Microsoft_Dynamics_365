fe9dbdd2f4147f1fd65a846cdd5218e11d296b55
AlexSan1910 <alex.gsan11@gmail.com>
2023-04-11 14:05:51 -0300

    Atualizações aula de sintaxe

--- Dominando .Net e JavaScript/03 Introdução ao .Net/criando classe no codigo/Program.cs
+++ Dominando .Net e JavaScript/03 Introdução ao .Net/criando classe no codigo/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using criando_classe_no_codigo.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();
